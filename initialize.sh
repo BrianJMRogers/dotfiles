d276678c08957123467136c76890ba271956490e
Brian Rogers <Brian@Brians-MacBook-Pro.local>
2017-11-24 16:55:52 -0500

    removing initialization script

--- initialize.sh
+++
@@ -1,9 +0,0 @@
-git init
-git add .
-git commit -m "first commit"
-git remote add origin git@github.com:BrianJMRogers/dotfiles.git
-git push -u origin master
-
-git rm inialize.sh
-gc -m "removing initialization script"
-git push
